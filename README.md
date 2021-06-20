9852a7a45b6fcb65427844b14e05f92c9a9d76ae
theonlyakhil <akhilnanosoft79@gmail.com>
2021-06-20 21:52:47 +0530

    added code and readme file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # UART_UDP_ESP32
-UART  to UDP for communicating with MAVLINK
+UART  to UDP for communicating with 
+
+
+#Connection 
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+RX(From telemetry Port of APM2.8) -> TX2 (ESP32)
+
+TX(from telemetry Port of APm 2.8 ->  RX2 (ESP32)
---
+++ wifiUDP_Serial.ino
@@ -0,0 +1,67 @@
+#include <WiFiUdp.h>
+#include <WiFi.h>
+
+
+
+const char* sta_ssid     = "saiPrasadam2.4g";
+const char* sta_password = "BC62D21070E0";
+
+unsigned int localUdpPort = 14555;
+char incomingPacket[255];
+#define BUFFER_SIZE 1024
+#define packTimeout 1
+//unsigned char buff[BUFFER_SIZE];
+WiFiUDP udp;
+byte buff[BUFFER_SIZE];
+
+
+
+void setup() {
+  Serial.begin(57600);
+  Serial2.begin(57600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(sta_ssid, sta_password);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial2.println("UDP Print");
+  udp.begin(localUdpPort);
+Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort); 
+}
+ 
+
+
+
+
+void loop() {
+  int size = 0;
+  int packetSize = udp.parsePacket();
+  char udpBuffer[packetSize];
+  
+  
+   
+    udp.read(udpBuffer,packetSize);
+    
+    Serial.write(udpBuffer,packetSize);
+    Serial2.write(udpBuffer,packetSize);
+   Serial2.flush();
+
+  
+    while( size = Serial2.available()){
+      size = (size >= BUFFER_SIZE ? BUFFER_SIZE : size);
+      Serial2.readBytes(buff, size);
+       udp.beginPacket(udp.remoteIP(), udp.remotePort());
+      udp.write(buff,size);
+       udp.endPacket();
+       
+    }
+    
+   }
